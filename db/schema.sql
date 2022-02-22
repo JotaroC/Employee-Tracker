1d4cf621d191d19134a57f1b3b7dbcd039b946ca
Alex C <cxz980314@gmail.com>
2022-02-21 20:33:54 -0500

    adding query.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,3 @@
--- GIVEN a command-line application that accepts user input
--- WHEN I start the application
--- THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
--- WHEN I choose to view all departments
--- THEN I am presented with a formatted table showing department names and department ids
--- WHEN I choose to view all roles
--- THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
--- WHEN I choose to view all employees
--- THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
--- WHEN I choose to add a department
--- THEN I am prompted to enter the name of the department and that department is added to the database
--- WHEN I choose to add a role
--- THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
--- WHEN I choose to add an employee
--- THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
--- WHEN I choose to update an employee role
--- THEN I am prompted to select an employee to update and their new role and this information is updated in the database
-
-
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
@@ -25,20 +6,18 @@ USE employee_db;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-
-    PRIMARY KEY(id)
-)
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE employee_role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
+    salary DECIMAL(9,2),
     department_id INT,
-
     PRIMARY KEY(id),
     FOREIGN KEY(department_id) REFERENCES department(id) 
     ON DELETE CASCADE
-)
+);
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
@@ -46,10 +25,9 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-
     PRIMARY KEY(id),
-    FOREIGN KEY(role_id) REFERENCES employee_role(id)
+    FOREIGN KEY (role_id) REFERENCES employee_role(id) 
     ON DELETE CASCADE,
-    FOREIGN KEY(manager_id) REFERENCES employee(manager_id)
-    ON DELETE SET NULL,
-)
+    FOREIGN KEY (manager_id) REFERENCES employee(id) 
+    ON DELETE SET NULL
+);
